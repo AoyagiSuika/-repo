79ceb5b7eb43b6468d42661248935edeea933cac
dahak_d <dahak.deborah@gmail.com>
2019-08-27 15:14:23 +0200

    test package

---
+++ package/package/fonctions.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: fonctions.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+"""module fonctions contenant la fonction table"""
+
+def table(nb, max = 10):
+    """Fonction affichant la table de multiplication par nb
+    de 1*nb à max*nb
+
+    (max >= 0)"""
+
+    i = 0
+
+    while i < max:
+        i += 1
+        print(i, "*", nb, "=", i * nb)
+
+if __name__ == "__main__":
+    table(4)
+    input("Appuyez sur ENTREE pour fermer le programme...")
---
+++ package/test_package.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: test_package.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+from package.fonctions import table
+
+table(5)
+
+# ou
+# import package.fonctions
+# package.fonctions.table(5)
