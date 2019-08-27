571684afe923847410135341d4d36c53fdb9a3bc
dahak_d <dahak.deborah@gmail.com>
2019-08-27 15:03:24 +0200

    module mult

---
+++ module/test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: test.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+from mult import *
+
+table(3, 20)
+input("Appuyez sur ENTREE pour fermer le programme...")
