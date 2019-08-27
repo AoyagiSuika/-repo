b38918270b30ce13ec50c8767182679684f3a831
dahak_d <dahak.deborah@gmail.com>
2019-08-27 17:58:51 +0200

    ZCasino v2

--- ZCasino/ZCasino.py
+++ ZCasino/ZCasino.py
@@ -20,43 +20,70 @@ def regles():
     print("Si la bille tombe sur votre nombre, vous récupérez")
     print("trois fois votre mise. Sinon, si la bille tombe sur")
     print("une case de la même couleur que votre nombre, vous")
-    print("récupérez la moitié de votre mise. Sinon, c'est perdu.")
+    print("récupérez la moitié de votre mise. Sinon, c'est perdu.\n")
 
-def roulette(nb, mise):
+def roulette(nb, mise, argent):
     """Fonction jouant le rôle de la roulette"""
 
     res = randrange(50)
     if res == nb:
         print("Félicitations, vous avez misé sur le bon nombre !")
         print("Vous remportez", 3 * mise, "$ !")
+        argent = argent + (3 * mise)
+        return argent
     elif res % 2 == nb % 2:
         print("Vous aviez la bonne couleur, vous récupérez", ceil(mise / 2),\
         "$, ce n'est pas si mal !")
+        argent = argent + (ceil(mise / 2))
+        return argent
     else:
         print("Perdu !")
+        return argent
 
-print("Bienvenue au ZCasino !")
+argent = 1000
+continuer = True
+
+print("Bienvenue au ZCasino ! Vous disposez de", argent, "$.\n")
 regles()
-nb = input("Sur quel nombre voulez-vous miser ?\n")
-try:
-    nb = int(nb)
-    if not (nb >= 0 and nb <= 49):
-        raise ValueError("Le nombre doit être compris entre 0 et 49.")
-except ValueError as error:
-    print(error)
-except TypeError:
-    print("Veuillez entrer un nombre entre 0 et 49.")
-else:
-    mise = input("Combien souhaitez-vous miser ?\n")
+while continuer == True and argent > 0:
+    nb = input("Sur quel nombre voulez-vous miser ?\n")
     try:
-        mise = int(mise)
-        if mise < 5:
-            raise ValueError("Votre mise doit être d'au moins 5$.")
+        nb = int(nb)
+        if not (nb >= 0 and nb <= 49):
+            raise ValueError("Le nombre doit être compris entre 0 et 49.")
     except ValueError as error:
         print(error)
     except TypeError:
-        print("Veuillez entrer un nombre valide.")
+        print("Veuillez entrer un nombre entre 0 et 49.")
     else:
-        print("Vous avez misé", mise, "$ sur le", nb,\
-        "lançons la roulette !")
-        roulette(nb, mise)
+        mise = input("Combien souhaitez-vous miser ?\n")
+        try:
+            mise = int(mise)
+            if mise < 5:
+                raise ValueError("Votre mise doit être d'au moins 5$.")
+            if mise > argent:
+                print("Vous n'avez pas autant d'argent.")
+                continue
+        except ValueError as error:
+            print(error)
+        except MoneyError as error:
+            print(error)
+        except TypeError:
+            print("Veuillez entrer un nombre valide.")
+        else:
+            print("\nVous avez misé", mise, "$ sur le", nb,\
+            "lançons la roulette !\n")
+            argent = argent - mise
+            argent = roulette(nb, mise, argent)
+    finally:
+        if argent > 0:
+            print("Il vous reste", argent, "$.")
+            rep = input("Souhaitez-vous rejouer ? Y/N\n")
+            if rep == "Y" or rep == "y":
+                print("C'est reparti !")
+            else:
+                print("Au revoir !")
+                continuer = False
+        else:
+            print("Vous êtes ruiné. Au revoir !")
+            continuer = False
