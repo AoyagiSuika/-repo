58dda9311b8ac47a94d372d4dc3672770e20e0b7
dahak_d <dahak.deborah@gmail.com>
2019-08-27 12:46:57 +0200

    basic.py + hello_world.py + bissextile.py

---
+++ basics/basic.py
@@ -0,0 +1,10 @@
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: basic.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
---
+++ bissextile/bissextile.py
@@ -0,0 +1,24 @@
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: bissextile.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+annee = input("Saisissez une année : ")
+annee = int(annee)
+
+if (annee % 4 == 0):
+    if (annee % 100 == 0):
+        if (annee % 400 == 0):
+            print(annee, "est une année bissextile.")
+        else:
+            print(annee, "n'est pas une année bissextile.")
+    else:
+        print(annee, "est une année bissextile.")
+else:
+    print(annee, "n'est pas une année bissextile.")
