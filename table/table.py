ffbe837ca86b71777dd7fdab3d943f1b7a000b47
dahak_d <dahak.deborah@gmail.com>
2019-08-27 12:57:07 +0200

    table.py

---
+++ table/table.py
@@ -0,0 +1,19 @@
+# @Author: Dahak Déborah <suika>
+# @Date:   27-08-2019
+# @Email:  dahak.deborah@gmail.com
+# @Project: Python3
+# @Filename: table.py
+# @Last modified by:   suika
+# @Last modified time: 27-08-2019
+
+#! /usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+nb = input("Entrez un nombre pour obtenir sa table de multiplication : ")
+nb = int(nb)
+
+i = 0
+
+while i < 10:
+    i += 1
+    print(i, "*", nb, "=", i * nb)
